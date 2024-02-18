0b0425e77d626c2fa1ba576bffcb718fc71f98ca
Pedro Freitas <132316286+ThanksJesus@users.noreply.github.com>
2024-02-18 17:37:22 -0300

    Update Final.sql

--- banco - SQL/Final.sql
+++ banco - SQL/Final.sql
@@ -11,6 +11,8 @@ mensagem text not null,
 primary key (id) 
 );
 
+DELETE FROM `contato`.`dados` WHERE (`id` = '');
+
 SELECT * FROM dados;
 
 CREATE TABLE cadastro (
@@ -21,22 +23,9 @@ senha varchar(40) not null,
 primary key (id) 
 );
 
+DELETE FROM `contato`.`cadastro` WHERE (`id` = '');
+
 SELECT * FROM cadastro;
 
-/* Parte que meche diretamente com os carrinhos */
-CREATE TABLE produtos (
-id int not null auto_increment,
-nome varchar(100) not null,
-valor decimal(10,2) not null,
-imagem varchar(50) not null,
-primary key (id)
-);
 
-
-SELECT * FROM produtos ;
-
-/*DELETE FROM `carrinho`.`produtos` WHERE (`id` = '2');*/
-
-INSERT INTO produtos (nome,valor,imagem) VALUES ('batata','12','image.png');
-INSERT INTO produtos (nome,valor,imagem) VALUES ('POTATO','16','image.png');
 
